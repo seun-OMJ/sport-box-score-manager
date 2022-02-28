d30b4e5d09a4bf047aa846348ea54ccbdc25ccdb
Jaspreet Singh <singhj43@myumanitoba.ca>
2022-02-28 04:18:07 +0000

    Updated MainActivity.java

--- Iteration1/SportBoxScoreManager/app/src/main/java/COMP3350/Sport_Box_Score_Manager/MainActivity.java
+++ Iteration1/SportBoxScoreManager/app/src/main/java/COMP3350/Sport_Box_Score_Manager/MainActivity.java
@@ -38,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                #something something
             }
         });
     }
