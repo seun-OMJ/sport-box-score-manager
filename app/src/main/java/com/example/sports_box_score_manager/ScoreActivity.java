57b0795a05a085a70ec96e2d23c5f69d6d8ea334
Seun OMJ <omojolaos@gmail.com>
2022-03-04 11:56:25 -0600

    player and game name is editable

--- app/src/main/java/com/example/sports_box_score_manager/ScoreActivity.java
+++ app/src/main/java/com/example/sports_box_score_manager/ScoreActivity.java
@@ -2,7 +2,9 @@ package com.example.sports_box_score_manager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ScoreActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class ScoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+        Intent intent = getIntent();
+        String text = intent.getStringExtra(PlayerActivity.extraText);
+        String text2 = intent.getStringExtra(PlayerActivity.extraText2);
+
+        TextView textView1 = findViewById(R.id.textView8);
+        TextView textView2 = findViewById(R.id.textView10);
+        textView1.setText(text);
+        textView2.setText(text2);
     }
 }
