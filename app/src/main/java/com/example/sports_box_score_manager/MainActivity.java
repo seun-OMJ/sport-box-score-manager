6941443337d2c8f3d244900b32b1b16978755191
Jaspreet Singh <singhj43@myumanitoba.ca>
2022-03-16 21:07:08 -0600

    Added Click Listerner for previous Game'

--- app/src/main/java/com/example/sports_box_score_manager/MainActivity.java
+++ app/src/main/java/com/example/sports_box_score_manager/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Click listener for Save And Conitnue button
         Button button = findViewById(R.id.button3);
         button.setOnClickListener(new OnClickListener() {
             @Override
@@ -24,17 +25,51 @@ public class MainActivity extends AppCompatActivity {
                 openActivity3();
             }
         });
+        // Click listeners for Previous Game buttons
+        Button PreviousGame1 = (Button) findViewById(R.id.button6);
+        Button PreviousGame2 = (Button) findViewById(R.id.button7);
+        Button PreviousGame3 = (Button) findViewById(R.id.button8);
+        PreviousGame1.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text1="Previous Game 1";
+                nextPage(text1);
+            }
+        });
+        PreviousGame2.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text2="Previous Game 2";
+                nextPage(text2);
+            }
+        });
+        PreviousGame3.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text3="Previous Game 3";
+                nextPage(text3);
+            }
+        });
     }
 
     public void openActivity3(){
         EditText edit1 = (EditText) findViewById(R.id.EnterNewGameName);
+
         String text = edit1.getText().toString();
+        Intent intent = new Intent(this, PlayerActivity.class);
+        //intent.putExtra(extraText, text);
+
         if(text.length()==0 | text.length()>25){
             edit1.setError("Invalid Input");
         }else {
-            Intent intent = new Intent(this, PlayerActivity.class);
             intent.putExtra(extraText, text);
-            startActivity(intent);
+            nextPage(text);
         }
     }
+    public void nextPage(String text){
+        Intent intent = new Intent(this, PlayerActivity.class);
+        intent.putExtra(extraText, text);
+        startActivity(intent);
+    }
+
 }
