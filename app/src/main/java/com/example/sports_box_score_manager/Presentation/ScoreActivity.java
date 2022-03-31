234941a41fcff07078699fe35b7be76ae57295e6
Jericho <dinozoj@myumanitoba.ca>
2022-03-31 03:08:17 -0500

    Added timer code

--- app/src/main/java/com/example/sports_box_score_manager/Presentation/ScoreActivity.java
+++ app/src/main/java/com/example/sports_box_score_manager/Presentation/ScoreActivity.java
@@ -1,7 +1,9 @@
 package com.example.sports_box_score_manager.Presentation;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,14 +14,27 @@ import com.example.sports_box_score_manager.Presentation.MainActivity;
 import com.example.sports_box_score_manager.Presentation.PlayerActivity;
 import com.example.sports_box_score_manager.R;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class ScoreActivity extends AppCompatActivity {
-            TextView score1,score2,score3,score4;
-            Button add1,sub1,add2,sub2,add3,sub3,add4,sub4, reset,save;
+            TextView score1,score2,score3,score4, timerText;
+            Button add1,sub1,add2,sub2,add3,sub3,add4,sub4, reset,save, timerStartBtn, timerResetBtn;
+
+            boolean timerStarted = false;
+            Timer timer;
+            TimerTask timerTask;
+            Double time = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+
+        timerText = (TextView) findViewById(R.id.text_view_timer);
+        timerStartBtn = (Button) findViewById(R.id.button_start);
+        timer = new Timer();
+
         Intent intent = getIntent();
         String text = intent.getStringExtra(PlayerActivity.extraText);
         String text2 = intent.getStringExtra(PlayerActivity.extraText2);
@@ -142,5 +157,72 @@ public class ScoreActivity extends AppCompatActivity {
         reset();
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void startBtnTapped(View view) {
+        if(timerStarted == false) {
+            timerStarted=true;
+            timerStartBtn.setText("pause");
+            startTimer();
+        } else {
+            timerStarted = false;
+            timerStartBtn.setText("start");
+            timerTask.cancel();
+        }
+    }
+
+    public void startTimer() {
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        time++;
+                        timerText.setText(getTimerText());
+                    }
+                });
+
+            }
+        };
+        timer.scheduleAtFixedRate(timerTask, 0, 1000);
+    }
+
+    public void resetTapped(View view) {
+        AlertDialog.Builder resetAlert = new AlertDialog.Builder(this);
+        resetAlert.setTitle("Reset Timer");
+        resetAlert.setMessage("Reset the timer?");
+        resetAlert.setPositiveButton("Reset", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(timerTask != null) {
+                    timerTask.cancel();
+                    time = 0.0;
+                    timerStarted = false;
+                    timerText.setText(formatTime(0,0,0));
+                }
+            }
+        });
+        resetAlert.setNeutralButton("cancel", new DialogInterface.OnClickListener() {
+           @Override
+           public void onClick(DialogInterface dialogInterface, int i) {
+
+           }
+        });
+        resetAlert.show();
+    }
+
+    private String getTimerText() {
+        int rounded = (int) Math.round(time);
+        int seconds = ((rounded % 86400) % 3600) % 60;
+        int minutes = ((rounded % 86400) % 3600) / 60;
+        int hours = ((rounded % 86400) / 3600);
+
+        return formatTime(seconds, minutes, hours);
+
+    }
+
+    private String formatTime(int secs, int mins, int hrs) {
+        return String.format("%02d", hrs) + " : " + String.format("%02d", mins) + " : " + String.format("%02d",secs);
     }
 }
