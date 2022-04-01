8558c51ae220c261fe60f57be8a19ad6d8a6f7d3
Seun OMJ <omojolaos@gmail.com>
2022-04-01 14:58:21 -0500

    broken the tests into smaller sections

--- app/src/test/java/com/example/sports_box_score_manager/logic/gameAccessTest.java
+++ app/src/test/java/com/example/sports_box_score_manager/logic/gameAccessTest.java
@@ -7,30 +7,31 @@ import com.example.sports_box_score_manager.Objects.GameModel;
 
 
 public class gameAccessTest {
+
+    GameAccess access = new GameAccess();
+    GameModel gameModel = new GameModel(0,"testGame","player1","player2","player3","player4");
     @Test
     public void create(){
         GameAccess access = new GameAccess();
         assertNotNull(access);
     }
     @Test
-    public void crudTest(){//testing insert, update and delete from hsqldb
-        GameAccess access = new GameAccess();
-        GameModel gameModel = new GameModel(0,"testGame","player1","player2","player3","player4");
-        assertEquals(access.insertGame(gameModel),gameModel);
+    public void insertTest(){//testing insert, update and delete from hsqldb
 
+        assertEquals(access.insertGame(gameModel),gameModel);}
+    @Test
+    public void updateTest(){
         gameModel.setGameName("editedName");
         gameModel.setPlayer1Name("edited_p1");
         gameModel.setPlayer2Name("edited_p2");
         gameModel.setPlayer3Name("edited_3");
         gameModel.setPlayer4Name("edited_4");
 
-        assertEquals(access.updateGame(gameModel),gameModel);
+        assertEquals(access.updateGame(gameModel),gameModel);}
+    @Test
+            public void deleteTest(){
         access.deleteGame(gameModel);
         assertEquals(access.getCurrentGame(),0);
-
-
-
-
     }
 
 
